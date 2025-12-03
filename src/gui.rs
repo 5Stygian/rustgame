c5ef2d05edeff0ed4930ab62b89b1a0bdaeb4ef2
5Stygian <rlcrafistohard@gmail.com>
2025-12-03 00:48:02 -0800

    rust is confuising

---
+++ src/gui.rs
@@ -0,0 +1,4 @@
+use macroquad::color::Color;
+
+// make a class for colors or something idk figure it out i need to hop on c.ai
+pub const GRAY: Color = Color { r: 0.5, g: 0.5, b: 0.5, a: 0.5 };
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
-}
+use macroquad::prelude::*;
+
+mod gui;
+
+#[macroquad::main("game")]
+pub async fn main() {
+    'mainloop: loop {
+        clear_background(gui::GRAY);
+
+        next_frame().await
+    } // mainloop
+} // main
