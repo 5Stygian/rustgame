4f84dd9c31379d1b1178febad03990ba91735c7d
5Stygian <rlcrafistohard@gmail.com>
2025-12-04 00:09:58 -0800

    change game engine

--- src/gui/colors.rs
+++
@@ -1,5 +0,0 @@
-use macroquad::prelude::*;
-
-pub fn rgba(r: f32, g: f32, b: f32, a: f32) -> Color {
-    Color::new(r, g, b, a)
-}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,46 @@
-use macroquad::prelude::*;
+use ggez::{ Context, ContextBuilder, GameResult };
+use ggez::graphics::{ self, Color, Rect };
+use ggez::event::{ self, EventHandler };
 
-use gui::colors;
+fn main() {
+    let (mut ctx, event_loop) = ContextBuilder::new("my_game", "5Stygian")
+        .build()
+        .expect("aieee, could not create ggez context!");
 
-#[macroquad::main("game")]
-pub async fn main() {
-    'mainloop: loop {
-        clear_background(gui::colors::rgba(0.8, 0.24, 0.1, 1.0));
+    let my_game = MyGame::new(&mut ctx);
 
-        next_frame().await
-    } // mainloop
-} // main
+    event::run(ctx, event_loop, my_game);
+} // fn main
+
+struct MyGame {
+} // MyGame
+
+impl MyGame {
+    pub fn new(_ctx: &mut Context) -> MyGame {
+        MyGame {
+        } // MyGame
+    } // fn new
+} // impl MyGame
+
+impl EventHandler for MyGame {
+    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+
+        Ok(())
+    } // fn update
+
+    fn draw(&mut self, ctx: &mut Context) -> GameResult {
+        let mut canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
+        
+        let rect = Rect::new(100.0, 100.0, 200.0, 200.0);
+        let mesh = graphics::Mesh::new_rectangle(
+            ctx,
+            graphics::DrawMode::fill(),
+            rect,
+            Color::from_rgb(255, 0, 0), // Red color
+        )?;
+
+        canvas.draw(&mesh, graphics::DrawParam::default());
+        
+        canvas.finish(ctx)
+    } // fn draw
+} // impl EventHandler for MyGame
