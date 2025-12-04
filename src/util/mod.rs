7e82c637905567bb1db3faf20fbd9e42824ea2cd
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-04 12:45:01 -0800

    make comment a rustdoc

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -2,7 +2,7 @@ use ggez::{ Context };
 use ggez::graphics::{ self, Color, Rect, Mesh };
 
 #[allow(non_snake_case)]
-// This is a helper function to make creating Rects more intuitive.
+/// This is a helper function to make creating Rects more intuitive.
 pub fn Rect(ctx: &mut Context, x: f32, y: f32, w: f32, h: f32, color: Color) -> Mesh {
     graphics::Mesh::new_rectangle(
         ctx,
