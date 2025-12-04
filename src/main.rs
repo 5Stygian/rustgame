9cbe7fa372bceb01589388c36a394e074872b6e1
5Stygian <rlcrafistohard@gmail.com>
2025-12-04 00:31:26 -0800

    create a basic Rect init function

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use ggez::{ Context, ContextBuilder, GameResult };
-use ggez::graphics::{ self, Color, Rect };
+use ggez::graphics::{ self, Color };
 use ggez::event::{ self, EventHandler };
+
+mod util;
 
 fn main() {
     let (mut ctx, event_loop) = ContextBuilder::new("my_game", "5Stygian")
@@ -31,15 +33,14 @@ impl EventHandler for MyGame {
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(ctx, Color::WHITE);
         
-        let rect = Rect::new(100.0, 100.0, 200.0, 200.0);
-        let mesh = graphics::Mesh::new_rectangle(
-            ctx,
-            graphics::DrawMode::fill(),
-            rect,
-            Color::from_rgb(255, 0, 0), // Red color
-        )?;
+        let rect = util::Rect(
+            ctx, 
+            100.0, 100.0, 
+            200.0, 300.0,
+            Color::from_rgb(255, 0, 0)
+        );
 
-        canvas.draw(&mesh, graphics::DrawParam::default());
+        canvas.draw(&rect, graphics::DrawParam::default());
         
         canvas.finish(ctx)
     } // fn draw
