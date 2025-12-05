3642716b752195a78b629c4f77eb76467e79eb5c
5Stygian <rlcrafistohard@gmail.com>
2025-12-05 00:37:20 -0800

    figured out a little more about how ContextBuilder and conf works

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,45 @@
 use ggez::{ Context, ContextBuilder, GameResult };
 use ggez::graphics::{ self, Color };
 use ggez::event::{ self, EventHandler };
+use ggez::conf::*;
 
 mod util;
+use util::color::CSS3;
 
 fn main() {
+    let window_mode = WindowMode {
+        width: 800.0,
+        height: 600.0,
+        maximized: false,
+        fullscreen_type: FullscreenType::Windowed,
+        borderless: false,
+        min_width: 1.0,
+        max_width: 0.0,
+        min_height: 1.0,
+        max_height: 0.0,
+        resizable: true,
+        visible: true,
+        transparent: false,
+        resize_on_scale_factor_change: false,
+        logical_size: None,
+    };
+    
+    let window_setup = WindowSetup {
+        title: "rustgame".to_owned(),
+        icon: "".to_owned(),
+        samples: NumSamples::One,
+        vsync: true,
+        srgb: true,
+    };
+
+    let conf = Conf {
+        window_mode: window_mode,
+        window_setup: window_setup,
+        backend: Backend::default(),
+    };
+
     let (mut ctx, event_loop) = ContextBuilder::new("rustgame", "5Stygian")
+        .default_conf(conf)
         .build()
         .expect("aieee, could not create ggez context!");
 
@@ -21,7 +55,7 @@ impl MyGame {
     pub fn new(_ctx: &mut Context) -> MyGame {
         MyGame {
         } // MyGame
-    } // fn new
+    } // pub fn new
 } // impl MyGame
 
 impl EventHandler for MyGame {
@@ -37,7 +71,7 @@ impl EventHandler for MyGame {
             ctx, 
             100.0, 100.0, 
             200.0, 300.0,
-            util::color::CSS3::RED,
+            CSS3::RED,
         );
 
         canvas.draw(&rect, graphics::DrawParam::default());
